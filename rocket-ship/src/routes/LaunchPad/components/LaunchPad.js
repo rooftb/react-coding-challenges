a9506b96477295012f598ca3585870e5b242a210
Tanner <tanner.roof@outlook.com>
2021-03-24 09:07:42 -0400

    Prevented the rocket from launching with a useEffect hook

--- rocket-ship/src/routes/LaunchPad/components/LaunchPad.js
+++ rocket-ship/src/routes/LaunchPad/components/LaunchPad.js
@@ -1,14 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ClassRocket, FunctionalRocket } from './Rocket';
 import '../styles/_launchpad.scss';
 
 export default function LaunchPad() {
   const [rerenderCount, triggerRerender] = useState(0);
 
-  setTimeout(() => { triggerRerender(rerenderCount + 1); }, 500);
+  useEffect(() => {
+    setTimeout(() => {
+      triggerRerender(rerenderCount + 1);
+    }, 500);
+  }, []);
 
   return (
-    <div className="launchpad">
+    <div className='launchpad'>
       <ClassRocket />
     </div>
   );
